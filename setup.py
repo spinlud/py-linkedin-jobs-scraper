d0208d1a8ee16cedf8905b3f0c54ae33d5594755
spinlud <ludovico.fabbri@gmail.com>
2023-05-28 19:47:11 +0200

    Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='linkedin-jobs-scraper',
-    version='2.1.0',
+    version='2.1.1',
     author='Ludovico Fabbri',
     author_email='ludovico.fabbri@gmail.com',
     description='Scrape public available jobs on Linkedin using headless browser',
